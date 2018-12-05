6a1adccf1cf0ec854bda23243f5c9f9adbc04296
niklasreinmuth <niklasreinmuth@users.noreply.github.com>
2018-12-05 08:14:14 +0100

    Update update_fs_requesters.ps1

--- freshservice/update_fs_requesters.ps1
+++ freshservice/update_fs_requesters.ps1
@@ -1,11 +1,15 @@
+# Authentication bei Freshservice
+# $api_key durch den API Key des Ticketerstellers ersetzen
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12
-$api_key = 'INSERT_API_KEY'
+$api_key = 'FRESHSERVICE API KEY' # API Key ersetzen
 $encoded_credentials = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $api_key, $null)))
 $http_headers = @{}
 $http_headers.add('Authorization', ("Basic {0}" -f $encoded_credentials))
 $http_headers.add('Content-Type', 'application/json')
 
-$uri = 'https://website.freshservice.com/api/v2/requesters'
+# API Connection URI
+$portal = 'WEBSITE'
+$uri = 'https://' + $portal + '.freshservice.com/api/v2/requesters'
 
 $list_of_all_requesters = Invoke-RestMethod -Method Get -Uri $uri -Headers $http_headers
 $table_of_requesters = $list_of_all_requesters | Select-Object -ExpandProperty requesters
@@ -13,7 +17,7 @@ $json_language_config = (Get-Content -Raw -Path '.\location_settings.json') | Co
 
 foreach ($requester in $table_of_requesters)
 {
-    $requester_profile_uri = 'https://website.freshservice.com/itil/requesters/' + $requester.id + '.json'
+    $requester_profile_uri = 'https://' + $portal + '.freshservice.com/itil/requesters/' + $requester.id + '.json'
     $requester_profile = Invoke-RestMethod -Method Get -Uri $requester_profile_uri -Headers $http_headers
     $requester_profile = $requester_profile | Select-Object -ExpandProperty user
 
